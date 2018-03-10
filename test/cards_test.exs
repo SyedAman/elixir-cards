0d9738c986b210483449d197206840dd4fc5c446
Syed Aman <sna274@nyu.edu>
2018-03-10 16:16:15 -0600

    add some tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,18 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "should create a deck of 52 cards" do
+    deck_of_cards = Cards.create_deck()
+    amount_of_cards = length(deck_of_cards)
+
+    assert amount_of_cards == 52
+  end
+
+  test "should shuffle a deck of cards" do
+    original_deck = Cards.create_deck()
+    shuffled_deck = Cards.shuffle(original_deck)
+
+    refute shuffled_deck == original_deck
+  end
 end
