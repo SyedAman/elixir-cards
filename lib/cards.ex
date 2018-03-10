f360ba392c9f6a42f0382f4692c16ff7fbc2dfb0
Syed Aman <sna274@nyu.edu>
2018-03-10 08:41:41 -0600

    use list comprehension to create a deck of cards which is a list of tuples

--- lib/cards.ex
+++ lib/cards.ex
@@ -13,16 +13,27 @@ defmodule Cards do
 
   """
 
-  def create_deck(deck) do
-    deck
+  def create_deck() do
+    suits = ["diamonds", "clubs", "spades", "hearts"]
+    ranks = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
+
+    for suit <- suits, rank <- ranks do
+      { rank, suit }
+    end
   end
 
   def shuffle_deck(deck) do
     Enum.shuffle(deck)
   end
 
+  def checkIfDeckHasCard?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
   def main do
-    newDeck = create_deck(["ace", "queen", "jack", "two"])
-    shuffle_deck(newDeck)
+    newDeck = create_deck()
+    # shuffledDeck = shuffle_deck(newDeck)
+
+    # checkIfDeckHasCard?(shuffledDeck, "ace")
   end
 end
